c520b2fb0ae8c2bcd43c0bb81b542fc65fb92c59
Louise Linné <linne.louise@gmail.com>
2021-06-01 15:15:00 +0200

    Add body-parser and compression

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
 
 const server = express();
 
 server.set('port', 3000);
+server.use(compression());
+server.use(bodyParser.json());
 
 export default server;
