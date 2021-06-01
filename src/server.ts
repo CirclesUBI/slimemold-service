563412997439840c2b4a5dba421b6bb430d111f4
Louise Linné <linne.louise@gmail.com>
2021-06-01 15:32:27 +0200

    Use cors, helmet and method-override

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import compression from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import methodOverride from 'method-override';
 
 const DEFAULT_PORT = 3000;
 
+// Initialize express instance
 const server = express();
 server.set('port', process.env.PORT || DEFAULT_PORT);
+
+// Use HTTP middlewares
 server.use(compression());
 server.use(bodyParser.json());
+server.use(methodOverride());
+
+// Use CORS and security middlewares
+server.use(cors());
+server.use(helmet());
 
 export default server;
