95dad54784d661d9ec9171b139b1d55077c7719e
Andreas Dzialocha <x12@adz.garden>
2021-06-17 14:23:44 +0200

    Use compression and helmet

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,23 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import compression from 'fastify-compress';
+import { fastifyHelmet } from 'fastify-helmet';
 
 import ServerModule from '~/server';
 
 async function bootstrap() {
+  // Create Fastify HTTP server instance
   const app = await NestFactory.create<NestFastifyApplication>(
     ServerModule,
     new FastifyAdapter(),
   );
 
+  // Register middlewares
+  app.register(compression);
+  app.register(fastifyHelmet);
+
+  // Start server
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
