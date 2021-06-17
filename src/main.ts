00a021cb7b9f1a96c512d03627f8cbdb6881e326
Andreas Dzialocha <x12@adz.garden>
2021-06-17 14:18:43 +0200

    Use fastify as server

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import ServerModule from '~/server';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ServerModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ServerModule,
+    new FastifyAdapter(),
+  );
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
