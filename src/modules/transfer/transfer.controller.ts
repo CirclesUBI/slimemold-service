f85ab7aa589938d96f5c23c7104bded5caeaa5d1
Andreas Dzialocha <x12@adz.garden>
2021-06-23 11:04:43 +0200

    Fix request data type

--- src/modules/transfer/transfer.controller.ts
+++ src/modules/transfer/transfer.controller.ts
@@ -1,5 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
-import { Controller, Get, Param, UsePipes } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes } from '@nestjs/common';
 
 import { FindTransferSteps } from '~/modules/transfer/transfer.dto';
 import TransferService from '~/modules/transfer/transfer.service';
@@ -10,7 +10,7 @@ export default class TransferController {
 
   @Get()
   @UsePipes(new ValidationPipe({ forbidUnknownValues: true }))
-  findTransferSteps(@Param() params: FindTransferSteps): string {
+  findTransferSteps(@Query() params: FindTransferSteps): string {
     return this.transferService.getHello();
   }
 }
