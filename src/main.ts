3133585b1f437522242a4236dee53b8b38789347
Andreas Dzialocha <x12@adz.garden>
2021-06-23 10:43:22 +0200

    Restructure main file a little

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,23 @@ import pkg from '../package.json';
 import ConfigService from '~/modules/config/config.service';
 import MainModule from '~/modules/main/main.module';
 
-async function startServer() {
+// Accept all IPv4 addresses when running the server.
+// See: https://www.fastify.io/docs/latest/Getting-Started
+const HOST_ADDRESS = '0.0.0.0';
+
+function registerOpenAPI(app: NestFastifyApplication, path = '/') {
+  const config = new DocumentBuilder()
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .setContact(pkg.name, pkg.homepage, null)
+    .setLicense(pkg.license, pkg.homepage)
+    .build();
+
+  SwaggerModule.setup(path, app, SwaggerModule.createDocument(app, config));
+}
+
+async function initializeServer(): Promise<NestFastifyApplication> {
   // Create Fastify HTTP server instance
   const app = await NestFactory.create<NestFastifyApplication>(
     MainModule,
@@ -33,23 +49,18 @@ async function startServer() {
     contentSecurityPolicy: false,
   });
 
-  // Setup OpenAPI endpoint
-  const openAPIConfig = new DocumentBuilder()
-    .setTitle(pkg.name)
-    .setDescription(pkg.description)
-    .setVersion(pkg.version)
-    .setContact(pkg.name, pkg.homepage, null)
-    .setLicense(pkg.license, pkg.homepage)
-    .build();
-  SwaggerModule.setup(
-    '/',
-    app,
-    SwaggerModule.createDocument(app, openAPIConfig),
-  );
+  return app;
+}
+
+async function startServer() {
+  const app = await initializeServer();
+
+  // Register public OpenAPI interface
+  registerOpenAPI(app);
 
   // Start server
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'), '0.0.0.0');
+  await app.listen(configService.get('PORT'), HOST_ADDRESS);
 }
 
 startServer();
